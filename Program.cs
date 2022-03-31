f0a3e68ad7113e95a2d15ef5573ad816e6352e0a
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-31 12:51:15 +0530

    Binary search

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+namespace AlgorithmProgram
+{
+    class Program
+    {
+        public void BinarySearch()
+        {
+            string[] words = { "mythili", "naveen", "lavanya", "santhosh", "kiruba" };
+            Array.Sort(words);
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+           int res= Array.BinarySearch(words,"naveen");
+            if(res>0)
+            {
+                Console.WriteLine(" index num is{0}.word is  founded",res);
+            }
+            else
+            {
+                Console.WriteLine("word is not in list");
+            }
+            
+        }
+        static void Main(string[]args)
+        {
+            Program program = new Program();
+            program.BinarySearch();
+        }
+    }
+
+}
